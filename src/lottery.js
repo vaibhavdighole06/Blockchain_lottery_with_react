4a0a2e0e1bef3d0fbce51e4efd16da710ccd21c6
vaibhavdighole06 <55737778+vaibhavdighole06@users.noreply.github.com>
2020-05-04 18:50:46 +0530

    Add files via upload

---
+++ src/lottery.js
@@ -0,0 +1,8 @@
+import web3 from './web3';
+
+const address = '0xC0F55E1Af868EE0BEd463D5c2D0288A10eeb14dD'
+
+//install pretier plugin for atom to look better
+const abi = [{"constant":true,"inputs":[],"name":"manager","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"pickWinner","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"getPlayers","outputs":[{"name":"","type":"address[]"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"enter","outputs":[],"payable":true,"stateMutability":"payable","type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"players","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"}]
+
+export default new web3.eth.Contract(abi, address); //export complete Contract
